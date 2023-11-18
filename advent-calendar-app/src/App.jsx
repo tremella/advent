7bd74a8fcb97fc5fb6e1c146b7715c975bd90612
Mayank Daswani <mdaswani@google.com>
2023-11-18 19:55:26 +0000

    Refactor rendering into the Calendar component.

--- advent-calendar-app/src/App.jsx
+++ advent-calendar-app/src/App.jsx
@@ -1,41 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Calendar from './components/Calendar/Calendar';
-import YoutubeContent from './components/YoutubeContent/YoutubeContent';
-import JavascriptAnimation from './components/JavascriptAnimation/JavascriptAnimation';
-import contentData from './data/contentData.js';
+import contentData from './data/contentData';
 
 function App() {
-  const [selectedDay, setSelectedDay] = useState(null);
-
-  const handleDayClick = day => {
-    setSelectedDay(day);
-  };
-
-  const renderContent = () => {
-    const dayData = contentData[selectedDay];
-    if (!dayData) return <div>Select a day</div>;
-
-    switch(dayData.type) {
-      case 'youtube':
-        return <YoutubeContent data={dayData} />;
-      case 'javascript':
-        return <JavascriptAnimation data={dayData} />;
-      // ... cases for other content types
-      default:
-        return <div>Content type not supported</div>;
-    }
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Advent Calendar</h1>
       </header>
       <main>
-        <Calendar onDayClick={handleDayClick} />
-        <div className="content-display">
-          {selectedDay !== null && renderContent()}
-        </div>
+        <Calendar contentData={contentData} />
       </main>
     </div>
   );
--- advent-calendar-app/src/components/Calendar/Calendar.jsx
+++ advent-calendar-app/src/components/Calendar/Calendar.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import './Calendar.css'; // Make sure to create a corresponding CSS file for styling
+import React, { useState } from 'react';
+import './Calendar.css';
+import YoutubeContent from '../YoutubeContent/YoutubeContent';
+import JavascriptAnimation from '../JavascriptAnimation/JavascriptAnimation';
+// ... import other content type components as needed
 
-const Calendar = ({ onDayClick }) => {
-  const days = Array.from({ length: 24 }, (_, i) => i + 1);
+const Calendar = ({ contentData }) => {
+  const [selectedDay, setSelectedDay] = useState(null);
+
+  const handleDayClick = day => {
+    setSelectedDay(day);
+  };
+
+  const renderContent = () => {
+    const dayData = contentData[selectedDay];
+    if (!dayData) return <div>Select a day</div>;
+
+    switch(dayData.type) {
+      case 'youtube':
+        return <YoutubeContent data={dayData} />;
+      case 'javascript':
+        return <JavascriptAnimation data={dayData} />;
+      // ... other content types
+      default:
+        return <div>Content type not supported</div>;
+    }
+  };
 
   return (
-    <div className="calendar">
-      {days.map(day => (
-        <div key={day} className="calendar-day" onClick={() => onDayClick(day)}>
-          {day}
-        </div>
-      ))}
+    <div>
+      <div className="calendar">
+        {Array.from({ length: 24 }, (_, i) => i + 1).map(day => (
+          <div key={day} className="calendar-day" onClick={() => handleDayClick(day)}>
+            {day}
+          </div>
+        ))}
+      </div>
+      <div className="content-display">
+        {selectedDay !== null && renderContent()}
+      </div>
     </div>
   );
 };
