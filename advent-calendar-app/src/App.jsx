876f9cc87f1151d6ae7313c69d031d42dff4d193
tremella <jeshbrand@gmail.com>
2023-11-18 20:07:37 +0000

    ok mayanks refac is now in place

--- advent-calendar-app/src/App.jsx
+++ advent-calendar-app/src/App.jsx
@@ -1,47 +1,18 @@
+import React from 'react';
+import Calendar from './components/Calendar/Calendar';
+import contentData from './data/contentData';
 
-import './App.css'
-
-
-import './index.css';
-
-const AdventCalendar = () => {
-  const days = Array.from({ length: 25 }, (_, index) => index + 1);
-
-  const getContentForDay = (day) => {
-    // Assume content items are stored in subdirectories named 1, 2, 3, ..., 25
-    try {
-      const content = require(`./content/${day}/content.json`);
-      return content;
-    } catch (error) {
-      console.error(`Error loading content for day ${day}:`, error);
-      return null;
-    }
-  };
-
-  const handleDayClick = (day) => {
-    // Placeholder function for handling day clicks
-    console.log(`Day ${day} clicked!`);
-  };
-
+function App() {
   return (
-    <div className="app">
-      <div className="advent-calendar">
-        {days.map(day => (
-          <div key={day} className="calendar-day" onClick={() => handleDayClick(day)}>
-            <span>{day}</span>
-            {getContentForDay(day) && (
-              <div className="content">
-                {/* Render your content here, e.g., content.title, content.image, etc. */}
-                <p>{getContentForDay(day).title}</p>
-                {/* Add more content elements as needed */}
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <h1>Advent Calendar</h1>
+      </header>
+      <main>
+        <Calendar contentData={contentData} />
+      </main>
     </div>
   );
-};
+}
 
-export default AdventCalendar;
-
+export default App;
