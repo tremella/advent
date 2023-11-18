73159851e6540d92e148c70c6981d029a29247c3
Mayank Daswani <mdaswani@google.com>
2023-11-18 21:53:14 +0000

    Working calendar animation, but rendered below the calendar.

--- advent-calendar-app/src/components/JavascriptAnimation/JavascriptAnimation.jsx
+++ advent-calendar-app/src/components/JavascriptAnimation/JavascriptAnimation.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const JavascriptAnimation = ({ data }) => {
-  // Render JavaScript animation using data.animationScript
-  return <div>{/* Animation content */}</div>;
+const JavascriptAnimation = ({ data }) => {    
+  const animationRef = useRef(null);  
+  let targetDivAndScriptLoaded = useRef(false);
+  useEffect(() => {
+    if (data.folder_location) {        
+        if (!targetDivAndScriptLoaded.current) {            
+            const targetDiv = document.createElement('div');
+            targetDiv.id = data.target_div || 'defaultId'; // Fallback to 'defaultId' if not provided
+            animationRef.current.appendChild(targetDiv);
+            targetDivAndScriptLoaded.current = true;
+
+            import(`../../data/${data.folder_location}/style.css`)
+            .then(() => {
+            // CSS is loaded, now you can load the script
+            const script = document.createElement('script');
+            script.src = `/src/data/${data.folder_location}/script.js`;
+            script.async = true;
+            animationRef.current.appendChild(script);
+
+            return () => {
+                animationRef.current.removeChild(script);
+                animationRef.current.removeChild(targetDiv);
+                targetDivAndScriptLoaded.current = false;
+            };
+            })
+            .catch(err => console.error('Error loading CSS:', err));
+        }
+    }
+  }, [data]);
+
+  return (
+    <div className="javascript-animation-container" ref={animationRef}>
+      {/* Other elements if needed */}
+    </div>
+  );
 };
 
 export default JavascriptAnimation;
--- advent-calendar-app/src/data/contentData.js
+++ advent-calendar-app/src/data/contentData.js
@@ -1,5 +1,8 @@
 const contentData = {
     "1" : {"folder_location": null, "type": "youtube", "details": {"comment": "Mariah Carey ChatRoulette.", "url": "https://www.youtube.com/watch?v=InYvRyX2Fu4"}}, 
-    "2" : {"folder_location": "src/content/xmas_cracker", "type": "javascript", "details": null}};
+    "2" : {"folder_location": "snowglobe",
+            "type": "javascript",
+            "target_div": "snowglobe"}
+};
 
 export default contentData;
