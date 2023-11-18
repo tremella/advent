cb8c2bf1ec02984821d6a8a01586661622427cf7
Mayank Daswani <mdaswani@google.com>
2023-11-18 22:48:14 +0000

    Add modal for displaying JavascriptAnimations.

--- advent-calendar-app/src/components/Calendar/Calendar.jsx
+++ advent-calendar-app/src/components/Calendar/Calendar.jsx
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
 import './Calendar.css';
+import Modal from 'react-modal';
 import YoutubeContent from '../YoutubeContent/YoutubeContent';
 import JavascriptAnimation from '../JavascriptAnimation/JavascriptAnimation';
 // ... import other content type components as needed
 
+Modal.setAppElement('#root'); // Set a root app element for accessibility
+
 const Calendar = ({ contentData }) => {
   const [selectedDay, setSelectedDay] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleDayClick = day => {
+    const dayData = contentData[day];
     setSelectedDay(day);
+
+    // Open modal only if the day's content is a JavascriptAnimation
+    if (dayData && dayData.type === 'javascript') {
+      setIsModalOpen(true);
+    }
   };
 
-  const renderContent = () => {
-    const dayData = contentData[selectedDay];
-    if (!dayData) return <div>Select a day</div>;
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const renderContent = (day) => {
+    const dayData = contentData[day];
+    if (!dayData) return null;
 
     switch(dayData.type) {
       case 'youtube':
@@ -22,22 +36,42 @@ const Calendar = ({ contentData }) => {
         return <JavascriptAnimation data={dayData} />;
       // ... other content types
       default:
-        return <div>Content type not supported</div>;
+        return null;
     }
   };
 
   return (
-    <div>
-      <div className="calendar">
-        {Array.from({ length: 24 }, (_, i) => i + 1).map(day => (
-          <div key={day} className="calendar-day" onClick={() => handleDayClick(day)}>
-            {day}
+    <div className="calendar">
+      {Array.from({ length: 24 }, (_, i) => i + 1).map(day => (
+        <div key={day} className="calendar-day" onClick={() => handleDayClick(day)}>
+          <div className="day-number">{day}</div>
+          {selectedDay === day && contentData[selectedDay] && contentData[selectedDay].type !== 'javascript' && (
+            <div className="content-container">
+              {renderContent(selectedDay)}
+            </div>
+          )}
+        </div>
+      ))}
+      {selectedDay !== null && contentData[selectedDay] && contentData[selectedDay].type === 'javascript' && (
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          style={{
+            overlay: {
+              backgroundColor: 'rgba(0, 0, 0, 0.5)'
+            },
+            content: {
+              backgroundColor: 'transparent',
+              border: 'none'          
+            }        
+          }}
+          contentLabel="JavaScript Animation Modal"
+        >
+          <div onClick={closeModal}>
+            {renderContent(selectedDay)}
           </div>
-        ))}
-      </div>
-      <div className="content-display">
-        {selectedDay !== null && renderContent()}
-      </div>
+        </Modal>
+      )}
     </div>
   );
 };
