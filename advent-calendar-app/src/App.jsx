8b0fe1475912c4307c9343794f7984ec25ed5c6b
tremella <jeshbrand@gmail.com>
2023-11-18 20:02:53 +0000

    deleted boilerplate code

--- advent-calendar-app/src/App.jsx
+++ advent-calendar-app/src/App.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
-import Calendar from './components/Calendar/Calendar';
-import contentData from './data/contentData';
 
-function App() {
+import './App.css'
+
+
+import './index.css';
+
+const AdventCalendar = () => {
+  const days = Array.from({ length: 25 }, (_, index) => index + 1);
+
+  const getContentForDay = (day) => {
+    // Assume content items are stored in subdirectories named 1, 2, 3, ..., 25
+    try {
+      const content = require(`./content/${day}/content.json`);
+      return content;
+    } catch (error) {
+      console.error(`Error loading content for day ${day}:`, error);
+      return null;
+    }
+  };
+
+  const handleDayClick = (day) => {
+    // Placeholder function for handling day clicks
+    console.log(`Day ${day} clicked!`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Advent Calendar</h1>
-      </header>
-      <main>
-        <Calendar contentData={contentData} />
-      </main>
+    <div className="app">
+      <div className="advent-calendar">
+        {days.map(day => (
+          <div key={day} className="calendar-day" onClick={() => handleDayClick(day)}>
+            <span>{day}</span>
+            {getContentForDay(day) && (
+              <div className="content">
+                {/* Render your content here, e.g., content.title, content.image, etc. */}
+                <p>{getContentForDay(day).title}</p>
+                {/* Add more content elements as needed */}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default AdventCalendar;
+
