e2b558939efe1890aff63dc496a270fc885e5849
tremella <jeshbrand@gmail.com>
2023-11-26 16:00:59 +0000

    jukebox img added

--- advent-calendar-app/src/components/Calendar/Calendar.jsx
+++ advent-calendar-app/src/components/Calendar/Calendar.jsx
@@ -59,6 +59,10 @@ const Calendar = ({ contentData }) => {
   };
 
   const renderContent = (day) => {
+
+    // this is where we feed in the data for a SPECIFIC day to our content components
+    // aka de-indexing 
+    
     const dayData = contentData[day];
     if (!dayData) return null;
     switch(dayData.type) {
