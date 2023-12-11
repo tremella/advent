882f75339e606fd5e2af2774a3fa5b379c5e24be
Mayank Daswani <mdaswani@google.com>
2023-12-11 22:00:57 +0000

    Fix clashing classes emoji classes.

--- advent-calendar-app/src/data/emoji_game/script.js
+++ advent-calendar-app/src/data/emoji_game/script.js
@@ -66,7 +66,7 @@ export function run() {
     emojiInputContainer.classList.add('emoji-input-container');
 
     const emojiDiv = document.createElement('div');
-    emojiDiv.classList.add('emoji');
+    emojiDiv.classList.add('emoji-icon');
     emojiDiv.textContent = item.emojis;
     emojiInputContainer.appendChild(emojiDiv);
 
